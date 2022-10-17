8b2614c49ef69fabc54f6021deac07f8465871df
Sami Aalto <torronen.sami@gmail.com>
2022-10-17 09:27:24 +0000

    Update 12 files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
-
+import { createRoot } from "react-dom/client";
+import "./translations/i18n";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>
+  </Router>
 );
